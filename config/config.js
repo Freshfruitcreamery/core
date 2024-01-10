32a86bb0dd2fb750a28fe95e8147d03724054078
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-01-10 11:15:24 +0100

    config updates

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
   },
   test: {
     username: process.env.DB_USER,
@@ -17,10 +17,11 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.PRODUCTION_DB_USER,
+    password: process.env.PRODUCTION_DB_PASS,
+    database: process.env.PRODUCTION_DB_NAME,
+    host: process.env.PRODUCTION_DB_HOST,
     dialect: 'mysql',
+    port: process.env.PRODUCTION_DB_PORT,
   },
 };
